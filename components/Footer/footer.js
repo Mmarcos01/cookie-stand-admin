79cc345d24ea9289d4e633f7c48de2c79f4dad93
Mmarcos01 <marie.marcos25@gmail.com>
2021-10-02 14:12:39 -0700

    feat: header component

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -1,5 +1,3 @@
-import Link from 'next/link'
-
 export default function Footer() {
     return(
         <footer className="p-4 bg-indigo-900 text-gray-50">
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { replies } from '../data'
 import { useState } from 'react'
 import Footer from '../components/Footer/footer'
+import Header from '../components/Header/header'
 
 export default function Home() {
   const[reply, setReply] = useState()
@@ -22,14 +23,7 @@ export default function Home() {
         <title>Cookie Stand Admin</title>
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <header className="flex items-center justify-between p-4 bg-indigo-900 text-gray-50 text-2xl mb-7">
-          <h1>Cookie Stand Admin</h1>
-          <nav className="flex items-center justify-left space-x-10">
-          <Link href="/about">
-            <a className="text-xl" href="about">Overview</a>
-          </Link>
-          </nav>
-      </header>
+    <Header />
      <main>
        <div className="mb-7 justify-between p-4 bg-indigo-200 w-3/5 m-auto rounded-lg">
          <h1 className="text-center text-2xl mb-4">Create Cookie Stand</h1>
