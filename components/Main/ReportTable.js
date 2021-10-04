7d9e63c566d9702ac0fce3a342cd2f21e91ffea5
Mmarcos01 <marie.marcos25@gmail.com>
2021-10-04 15:57:55 -0700

    feat: edit report table

--- components/Main/ReportTable.js
+++ components/Main/ReportTable.js
@@ -1,11 +1,25 @@
+import { hours } from '../../data'
+
 export default function ReportTable(props) {
+  // if (props.tableData.length === 0) {
+  //   return <h2 className='my-8 text-xl text-center'>No Cookie Stands Available</h2>;
+  // }
     return(
         <table className="w-1/2 mx-auto my-4">
-        <tr>
-          <th>Minimum Customers per Hour</th>
-          <th>Maximum Customers per Hour</th>
-          <th>Average Cookies per Sale</th>
-        </tr>
+        <thead className='bg-gray-800 text-white'>
+          <tr>
+            <th className='p-4 text-left'>Location</th>
+            {hours.map((hour) => {
+              return (
+                <th className='px-3'>
+                  {hour}
+                </th>
+              );
+            })}
+            <th className='px-4'>Totals</th>
+          </tr>
+        </thead>
+    
         {props.tableData.map(item => {
               return(<tr className="odd:bg-gray-200">
           <td className="text-center border border-indigo-300 bg-gray-50">x</td>
--- data.js
+++ data.js
@@ -1,10 +1,1 @@
-// export const replies = [
-//     {
-//     location:"Barcelona",
-//     minCustomers:2,
-//     maxCustomers:4,
-//     avgCookies:2.5
-//     }
-// ];
-
 export const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
