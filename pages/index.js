1d4d5bc0b9c468c4f88bf4e87d7c7a999bfc63d4
Mmarcos01 <marie.marcos25@gmail.com>
2021-10-06 19:34:02 -0700

    hooks resource

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,39 @@
-import CookieStandAdmin from '../components/CookieStandAdmin/CookieStandAdmin'
+import CookieStandAdmin from "../components/CookieStandAdmin/CookieStandAdmin";
+import { useAuth } from "../contexts/auth";
 
 export default function Home() {
+  const { user, login, logout } = useAuth();
 
   return (
     <div>
-      <CookieStandAdmin />
+      {/* <CookieStandAdmin /> */}
+      {user ? (
+        <>
+          <h2 className="text-4xl">Welcome {user.username}</h2>
+          <CookieStandAdmin />
+          <button
+            onClick={logout}
+            className="p-2 text-white bg-gray-500 rounded"
+          >
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <h2>Need to log in</h2>
+          <button
+            onClick={() =>
+              login(
+                process.env.NEXT_PUBLIC_USERNAME,
+                process.env.NEXT_PUBLIC_PASSWORD
+              )
+            }
+            className="p-2 text-white bg-gray-500 rounded"
+          >
+            Login
+          </button>
+        </>
+      )}
     </div>
-  )
+  );
 }
